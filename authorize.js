629d431519f971978f73fe839f11f0d65eae5d23
Michael <mjj.yoyogi@gmail.com>
2023-05-24 00:00:04 -0400

    Added a lot, discord connection iffy.

--- authorize.js
+++ authorize.js
@@ -1,13 +1,12 @@
 const clientID = '1110754146988085378'
 const redirectUri = 'https://google.com'
 
-const authorizationUrl = 'https://discord.com/api/oauth2/authorize?client_id=1110754146988085378&permissions=414464658496&redirect_uri=https%3A%2F%2Fwww.google.com%2F&response_type=code&scope=guilds%20guilds.join%20guilds.members.read%20activities.read%20activities.write%20dm_channels.read%20role_connections.write%20applications.commands.permissions.update%20relationships.read%20applications.builds.upload%20applications.builds.read%20messages.read%20applications.entitlements%20voice%20applications.store.update%20applications.commands%20webhook.incoming%20bot%20rpc.activities.write%20rpc.screenshare.write%20rpc.screenshare.read%20rpc.video.write%20rpc.video.read%20rpc.voice.write%20rpc.voice.read%20rpc.notifications.read%20rpc%20gdm.join%20connections%20email%20identify'
-
+const authorizationUrl = 'https://discord.com/api/oauth2/authorize?client_id=1110754146988085378&permissions=2147534912&redirect_uri=https%3A%2F%2Fwww.google.com%2F&response_type=code&scope=identify%20guilds%20gdm.join%20rpc.voice.read%20rpc.video.write%20rpc.activities.write%20messages.read%20applications.commands%20activities.read%20voice%20applications.commands.permissions.update%20dm_channels.read%20activities.write%20applications.builds.upload%20rpc.screenshare.read%20rpc.voice.write%20rpc%20guilds.join%20email%20bot%20applications.store.update%20role_connections.write%20relationships.read%20applications.builds.read%20webhook.incoming%20applications.entitlements%20rpc.screenshare.write%20rpc.video.read%20guilds.members.read%20connections%20rpc.notifications.read'
 // Extract access token from url hash fragment
 
 const params = window.location.hash.substring(1).split('&');
 
-const accessToken = params.reduce((result, item) => {
+const TOKEN = params.reduce((result, item) => {
     const [key, value] = item.split('=');
     if (key === 'access_token'){
         result = decodeURIComponent(value);
@@ -15,46 +14,91 @@ const accessToken = params.reduce((result, item) => {
     return result
 }, '');
 
-fetch('https://discord.com/api/v10/users/@me/guilds', {
-    headers: {
-        'Authorization': `Bearer ${accessToken}`
-    }
-})
-    .then(response => response.json())
-    .then(guilds => {
-        if (guilds.length > 0){
-            const guildId = guilds[0].id
-            //another api request to retrieve channels of the guild
-            fetch(`https://discord.com/api/v10/guilds${guildId}/channels`, {
-                headers:{
-                    'Authorization': `Bearer ${accessToken}`
+const DISCORD_API_URL = 'https://discord.com/api/v10'
+const GATEWAY_URL = "wss://gateway.discord.gg/?v=10&encoding=json"
+const GUILD_ID = '1110757419644100649'
+const CHANNEL_ID = '1110757420256481315'
+
+
+// dom elements
+const chatDisplay = document.getElementById('chatDisplay');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+
+
+// websocket connection
+
+const socket = new WebSocket(GATEWAY_URL);
+socket.addEventListener('message', handleGatewayMessage)
+
+//Event listeners
+
+sendButton.addEventListener('click', sendMessage)
+
+//functions
+
+function handleGatewayMessage(event){
+    const message = JSON.parse(event.data);
+
+    // Handle diff message types
+    switch (message.op){
+        case 10:
+            socket.send(JSON.stringify({
+                op:2,
+                d: {
+                    token: TOKEN,
+                    intents: 513,
+                    properties: {
+                        $os: 'windows',
+                        $browser: 'chrome',
+                        $device: 'chrome'
+                    }
                 }
-            })
-                .then(response => response.json())
-                .then(channels => {
-                    const channelId = channels[0].id
+            }));
+            break;
+        case 0:
+            if (message.t === 'MESSAGE_CREATE'){
+                const chatMessage = message.d;
+                displayChatMessage(chatMessage);
 
-                    console.log('Access token: ', accessToken)
-                    console.log('Guild id: ', guildId)
-                    console.log('Channel id: ', channelId)
+            }
+            break;
 
-                })
-                .catch(error => {
-                    console.error('failed to fetch channels: ', error)
-                });
+        case 9:
+            //reconnect if sesion invalidated
+            socket.send(JSON.stringify({
+                op:6,
+                d: {
+                    token: TOKEN,
+                    session_id: message.d.session_id,
+                    seq: message.s
+                }
+            }));
+            break;
+    }
+}
 
-        }else{
-            console.log(guilds)
-            console.error('user not in guilds')
-        }
+// Function to display chat msgs
 
+function displayChatMessage(message){
+    const messageElement = document.createElement('p');
+    messageElement.textContent = `${message.author.username}: ${message.content}`;
+    chatDisplay.appendChild(messageElement)
 
-        
-    })
-    .catch(error => {
-        console.error('failed to fetch guilds: ', error)
-    })
+}
 
+// function to send a message to the discord chat
 
-
-
+function sendMessage(){
+    const message = messageInput.value;
+    if (message.trim() !== ''){
+        fetch(`${DISCORD_API_URL}/channels/${CHANNEL_ID}/messages`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${TOKEN}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON
+        })
+    }
+}
